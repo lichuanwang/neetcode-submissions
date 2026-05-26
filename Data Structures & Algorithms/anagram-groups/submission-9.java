85490a351fbc922ac0683731f7cf356d9e799261
lichuanwang <136982952+lichuanwang@users.noreply.github.com>
2026-05-26 17:47:11 +0800

    Add: anagram-groups - submission-9

---
+++ Data Structures & Algorithms/anagram-groups/submission-9.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String s : strs) {
+            int[] count = new int[26];
+            for (char c : s.toCharArray()) {
+                count[c - 'a']++;
+            }
+
+            String key = Arrays.toString(count);
+            if (map.containsKey(key)) {
+                map.get(key).add(s);
+            } else {
+                List<String> list = new ArrayList<>();
+                list.add(s);
+                map.put(key, list);
+            }
+        }
+
+        List<List<String>> result = new ArrayList<>();
+        for (List<String> list : map.values()) {
+            result.add(list);
+        }
+
+        return result;
+    }
+}
