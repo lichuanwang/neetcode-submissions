759222460c5e59e3ac91ec638fa39f48d902c274
lichuanwang <136982952+lichuanwang@users.noreply.github.com>
2026-05-26 14:14:17 +0800

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.java
@@ -0,0 +1,55 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length()) {
+            return false;
+        }
+
+        int[] s1Count = new int[26];
+        int[] s2Count = new int[26];
+        for (int i = 0; i < s1.length(); i++) {
+            char s1C = s1.charAt(i);
+            char s2C = s2.charAt(i);
+            s1Count[s1C - 'a']++;
+            s2Count[s2C - 'a']++;
+        }
+
+        int matches = 0;
+        for (int i = 0; i < 26; i++) {
+            if (s1Count[i] == s2Count[i]) {
+                matches++;
+            }
+        }
+
+        int l = 0;
+        for (int r = s1.length(); r < s2.length(); r++) {
+
+            if (matches == 26) {
+                return true;
+            }
+
+            char c = s2.charAt(r);
+            int index = c - 'a';
+            s2Count[index]++;
+            
+            if (s2Count[index] - 1 == s1Count[index]) {
+                matches--;
+            } else if (s2Count[index] == s1Count[index]) {
+                matches++;
+            }
+            
+            c = s2.charAt(l);
+            index = c - 'a';
+            s2Count[index]--;
+
+            if (s2Count[index] + 1 == s1Count[index]) {
+                matches--;
+            } else if (s2Count[index] == s1Count[index]) {
+                matches++;
+            }
+
+            l++;
+        }
+
+        return matches == 26;
+    }
+}
